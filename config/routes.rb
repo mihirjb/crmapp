f30a31d46b4e5fe3318490d1336f442a1ec223ce
Mihir Buch <mihirjb@gmail.com>
2012-11-21 20:15:29 +0530

    Added the correct path for the pages

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,13 @@ Crmapp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  root :to => "pages#home"
+  
+  match '/contact', :to => "pages#contact"
+  match '/about', :to => "pages#about"
+  match '/signup', :to => "users#new"
+  
+  
+  
+  
 end
