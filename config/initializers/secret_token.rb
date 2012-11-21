416cd481616d870354011a53b46a2dbcdb613037
Mihir Buch <mihirjb@gmail.com>
2012-11-21 19:38:17 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crmapp::Application.config.secret_token = '723f7636855420c508f05787b52da50c79d1fd1c727d561a74ceb65e782e88f43463b6510c590083e5a0dec596b46848bf89e76bdf1369d1871c73458c0d5b67'
